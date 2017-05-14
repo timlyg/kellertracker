6e0969d405dd11c5e1e07b0fe26ae390e3fda24f
Timothy Law <timlyg@yahoo.com>
2017-05-14 08:33:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
   <body>
   <h1>Welcome to Keller Tracker</h1>
   This site keeps track of the weekly schedule of Redeemer.<br>
-  Since Redeemer does not announce the preacher's schedule before hand, this site helps those in need of this information.<br>
+  Since Redeemer does not announce the preacher's schedule before hand, this site helps those in need of this information.<br><br>
+    
   Today is:
   <?php
     echo date("Y/m/d");
