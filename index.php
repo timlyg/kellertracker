4cb455ad8735c23553b66e57d5d503c83d6ff172
Timothy Law <timlyg@yahoo.com>
2017-05-14 08:32:17 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+  <title>
+    Keller Tracker
+  </title>
+  </head>
+  
+  <body>
+  <h1>Welcome to Keller Tracker</h1>
+  This site keeps track of the weekly schedule of Redeemer.<br>
+  Since Redeemer does not announce the preacher's schedule before hand, this site helps those in need of this information.<br>
+  Today is:
+  <?php
+    echo date("Y/m/d");
+  ?>
+  Timothy Keller is/will be preaching at
+  
+  </body>
+</html>
