594be2419f87b1c2478020578474ba5215ae51d7
Timothy Law <timlyg@yahoo.com>
2017-05-16 14:04:12 -0400

    added stats

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <html>
   <head>
-     <!-- Start of StatCounter Code for Default Guide -->
-    <script type="text/javascript">
-    var sc_project=11343304; 
-    var sc_invisible=1; 
-    var sc_security="a36c042c"; 
-    var scJsHost = (("https:" == document.location.protocol) ?
-    "https://secure." : "http://www.");
-    document.write("<sc"+"ript type='text/javascript' src='" +
-    scJsHost+
-    "statcounter.com/counter/counter.js'></"+"script>");
-    </script>
-    <noscript><div class="statcounter"><a title="web analytics"
-    href="http://statcounter.com/" target="_blank"><img
-    class="statcounter"
-    src="//c.statcounter.com/11343304/0/a36c042c/1/" alt="web
-    analytics"></a></div></noscript>
-    <!-- End of StatCounter Code for Default Guide -->
   <title>
     Keller Tracker
   </title>
@@ -33,5 +16,24 @@
   ?>
   Timothy Keller is/will be preaching at
   
+    
+    
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=11343304; 
+var sc_invisible=1; 
+var sc_security="a36c042c"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="web counter"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="//c.statcounter.com/11343304/0/a36c042c/1/" alt="web
+counter"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
   </body>
 </html>
